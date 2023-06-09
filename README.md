7659011175157cf7911072538afbfc1f13458a21
Chenxi <chenxi.whitehouse@gmail.com>
2023-06-09 14:53:32 +0000

    replicate

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Stable Diffusion v2 Cog model
+# DiffEdit Stable Diffusion Cog model
 
-[![Replicate](https://replicate.com/stability-ai/stable-diffusion/badge)](https://replicate.com/stability-ai/stable-diffusion) 
+[![Replicate](https://replicate.com/cjwbw/diffedit-stable-diffusion/badge)](https://replicate.com/cjwbw/diffedit-stable-diffusion) 
 
-This is an implementation of the [Diffusers Stable Diffusion v2.1](https://huggingface.co/stabilityai/stable-diffusion-2-1) as a Cog model. [Cog packages machine learning models as standard containers.](https://github.com/replicate/cog)
+This is an implementation of the [DiffEdit-stable-diffusion](https://github.com/Xiang-cd/DiffEdit-stable-diffusion) as a Cog model. [Cog packages machine learning models as standard containers.](https://github.com/replicate/cog)
 
 First, download the pre-trained weights:
 
@@ -10,4 +10,4 @@ First, download the pre-trained weights:
 
 Then, you can run predictions:
 
-    cog predict -i prompt="monkey scuba diving"
+    cog predict -i image=...
--- predict.py
+++ predict.py
@@ -1,73 +1,52 @@
 import os
-from typing import List
-
 import torch
+from PIL import Image
 from cog import BasePredictor, Input, Path
 from diffusers import (
-    StableDiffusionPipeline,
-    PNDMScheduler,
-    LMSDiscreteScheduler,
     DDIMScheduler,
-    EulerDiscreteScheduler,
-    EulerAncestralDiscreteScheduler,
-    DPMSolverMultistepScheduler,
-)
-from diffusers.pipelines.stable_diffusion.safety_checker import (
-    StableDiffusionSafetyChecker,
+    DDIMInverseScheduler,
+    StableDiffusionDiffEditPipeline,
 )
 
 
-MODEL_ID = "stabilityai/stable-diffusion-2-1"
 MODEL_CACHE = "diffusers-cache"
-SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading pipeline...")
-        safety_checker = StableDiffusionSafetyChecker.from_pretrained(
-            SAFETY_MODEL_ID,
+        sd_model_ckpt = "stabilityai/stable-diffusion-2-1"
+        self.pipeline = StableDiffusionDiffEditPipeline.from_pretrained(
+            sd_model_ckpt,
             cache_dir=MODEL_CACHE,
-            local_files_only=True,
-        )
-        self.pipe = StableDiffusionPipeline.from_pretrained(
-            MODEL_ID,
-            safety_checker=safety_checker,
-            cache_dir=MODEL_CACHE,
+            torch_dtype=torch.float16,
+            safety_checker=None,
             local_files_only=True,
         ).to("cuda")
+        self.pipeline.scheduler = DDIMScheduler.from_config(
+            self.pipeline.scheduler.config
+        )
+        self.pipeline.inverse_scheduler = DDIMInverseScheduler.from_config(
+            self.pipeline.scheduler.config
+        )
+        self.pipeline.enable_model_cpu_offload()
+        self.pipeline.enable_vae_slicing()
 
     @torch.inference_mode()
     def predict(
         self,
-        prompt: str = Input(
-            description="Input prompt",
-            default="a photo of an astronaut riding a horse on mars",
-        ),
-        negative_prompt: str = Input(
-            description="Specify things to not see in the output",
+        image: Path = Input(
+            description="Input image",
             default=None,
         ),
-        width: int = Input(
-            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=768,
+        source_prompt: str = Input(
+            description="The prompt to guide the semantic mask generation using the method used in the DiffEdit paper",
+            default="a bowl of fruits",
         ),
-        height: int = Input(
-            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=768,
-        ),
-        prompt_strength: float = Input(
-            description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
-            default=0.8,
-        ),
-        num_outputs: int = Input(
-            description="Number of images to output.",
-            ge=1,
-            le=4,
-            default=1,
+        target_prompt: str = Input(
+            description="The prompt to guide the target image generation",
+            default="a basket of fruits",
         ),
         num_inference_steps: int = Input(
             description="Number of denoising steps", ge=1, le=500, default=50
@@ -75,70 +54,40 @@ class Predictor(BasePredictor):
         guidance_scale: float = Input(
             description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
         ),
-        scheduler: str = Input(
-            default="DPMSolverMultistep",
-            choices=[
-                "DDIM",
-                "K_EULER",
-                "DPMSolverMultistep",
-                "K_EULER_ANCESTRAL",
-                "PNDM",
-                "KLMS",
-            ],
-            description="Choose a scheduler.",
-        ),
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
-    ) -> List[Path]:
+    ) -> Path:
         """Run a single prediction on the model"""
         if seed is None:
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
-
-        if width * height > 786432:
-            raise ValueError(
-                "Maximum size is 1024x768 or 768x1024 pixels, because of memory limits. Please select a lower width or height."
-            )
-
-        self.pipe.scheduler = make_scheduler(scheduler, self.pipe.scheduler.config)
-
         generator = torch.Generator("cuda").manual_seed(seed)
-        output = self.pipe(
-            prompt=[prompt] * num_outputs if prompt is not None else None,
-            negative_prompt=[negative_prompt] * num_outputs
-            if negative_prompt is not None
-            else None,
-            width=width,
-            height=height,
+
+        raw_image = Image.open(str(image)).convert("RGB").resize((768, 768))
+        # employ the source and target prompts to generate the editing mask
+        mask_image = self.pipeline.generate_mask(
+            image=raw_image,
+            source_prompt=source_prompt,
+            target_prompt=target_prompt,
+            generator=generator,
+            guidance_scale=guidance_scale,
+        )
+        # employ the caption and the input image to get the inverted latents:
+        inv_latents = self.pipeline.invert(
+            prompt=source_prompt, image=raw_image, generator=generator
+        ).latents
+
+        # generate the image with the inverted latents and semantically generated mask:
+        image = self.pipeline(
+            prompt=target_prompt,
+            mask_image=mask_image,
+            image_latents=inv_latents,
+            negative_prompt=source_prompt,
             guidance_scale=guidance_scale,
             generator=generator,
-            num_inference_steps=num_inference_steps,
-        )
+        ).images[0]
+        output_path = f"/tmp/out.png"
+        image.save(output_path)
 
-        output_paths = []
-        for i, sample in enumerate(output.images):
-            if output.nsfw_content_detected and output.nsfw_content_detected[i]:
-                continue
-
-            output_path = f"/tmp/out-{i}.png"
-            sample.save(output_path)
-            output_paths.append(Path(output_path))
-
-        if len(output_paths) == 0:
-            raise Exception(
-                f"NSFW content detected. Try running it again, or try a different prompt."
-            )
-
-        return output_paths
-
-
-def make_scheduler(name, config):
-    return {
-        "PNDM": PNDMScheduler.from_config(config),
-        "KLMS": LMSDiscreteScheduler.from_config(config),
-        "DDIM": DDIMScheduler.from_config(config),
-        "K_EULER": EulerDiscreteScheduler.from_config(config),
-        "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(config),
-        "DPMSolverMultistep": DPMSolverMultistepScheduler.from_config(config),
-    }[name]
+        return Path(output_path)
